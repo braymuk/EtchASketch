bdc934bc58c59a92df6236e0e4cbfeb9afadf442
braymuk <braydeng9@gmail.com>
2021-07-01 18:14:32 -0700

    Added button and page styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const container = document.querySelector('#container');
 
+//Creating grid
 for(i = 1; i <= 16; i ++) {
     for(j = 1; j <= 16; j++) {
         const square = document.createElement('div');
@@ -12,11 +13,31 @@ for(i = 1; i <= 16; i ++) {
         square.style.gridRow = j;
 
         square.addEventListener('mouseover', ()=> {
-            square.style.backgroundColor = 'blue';
+            square.classList.add('filledSquare');
         });
-
         container.appendChild(square);
-        
     }
-    
 }
+
+const resetCanvasButton = document.querySelector('#resetButton');
+
+resetCanvasButton.addEventListener('click', ()=> {
+    do{
+        var canvasSize = window.prompt("Enter the new grid size.");
+        if(canvasSize > 100) {
+            window.alert("Please select a value less than 100");
+        }
+    } while(canvasSize > 100);
+
+    const squares = document.querySelectorAll('.filledSquare');
+    squares.forEach((e)=> {
+       e.classList.remove('filledSquare');
+    });
+
+    for(i = 1; i <= canvasSize; i++) {
+        for (j = 1; j < canvasSize; j++) {
+
+
+        }
+    }
+});
