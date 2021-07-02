6290449de099addcfe23b88656e20d723bd2d8ef
braymuk <braydeng9@gmail.com>
2021-07-01 17:49:46 -0700

    Added hover effect

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ for(i = 1; i <= 16; i ++) {
     for(j = 1; j <= 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+
+        //Placement
         square.style.gridColumn = i;
         square.style.gridRow = j;
-      
+
+        square.addEventListener('mouseover', ()=> {
+            square.style.backgroundColor = 'blue';
+        });
 
         container.appendChild(square);
         
